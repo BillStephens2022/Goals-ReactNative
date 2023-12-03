5f29735b45b34dbcd7ec8de5a18a6cbe95a4519d
BillStephens2022 <two4onebill@yahoo.com>
2023-12-03 08:05:15 -0500

    added ripple effect upon deleting a goal

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem({ id, text, onDeleteItem }) {
   return (
-    <Pressable onPress={onDeleteItem.bind(this, id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={onDeleteItem.bind(this, id)}
+        style={({pressed}) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -15,12 +19,15 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
     color: "white",
   },
+  pressedItem: {
+    opacity: 0.5
+  },
   goalText: {
+    padding: 8,
     color: "white",
   },
 });
